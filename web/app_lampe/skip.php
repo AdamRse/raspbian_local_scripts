6d2c7412a3bf0f57f935549f435f8e873dfbda68
AdamRse <adamrse@gmail.com>
2025-12-15 16:05:34 +0100

    fix skip.php

--- web/app_lampe/skip.php
+++ web/app_lampe/skip.php
@@ -1,10 +1,10 @@
 <?php
-require '/var/www/bibliotheque/pi/intrusion.php';
+// require '/var/www/bibliotheque/pi/intrusion.php';
 
-if(!intrus){
+// if(!intrus){
     $msqli=new mysqli("localhost", "raspi", "", "raspi_general");
     if($msqli->query("UPDATE opt SET valeur = ".((isset($_GET['o']) && ($_GET['o']=="1" || $_GET['o']=="2"))?$_GET['o']:0)." WHERE nom_opt = 'lampe_run_skip'"))
         echo "1";
     else
         echo "0";
-}
+//}
