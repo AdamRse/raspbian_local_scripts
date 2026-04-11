98689dd054191eca5ebce0a3c411e7a558f0a577
AdamRse <adam.rousselle@proton.me>
2026-04-11 18:11:14 +0200

    correxion syntaxique

--- lampe_switch_pi_OS.sh
+++ lampe_switch_pi_OS.sh
@@ -6,39 +6,39 @@
 ############
 
 # Conversion BCM vers SYSFS (nouvelle numérotation)
-GPIO_SYSFS=$((512 + $1))
+GPIO_SYSFS=$((512 + ${1}))
 
 # normal=true va servir à déterminer si le gpio a été fermé correctement de façon à éviter un double switch
 normal=true
 
-if [ ! -f "/sys/class/gpio/gpio$GPIO_SYSFS/value" ]; then
-  echo "$GPIO_SYSFS" > /sys/class/gpio/export
+if [ ! -f "/sys/class/gpio/gpio${GPIO_SYSFS}/value" ]; then
+  echo "${GPIO_SYSFS}" > /sys/class/gpio/export
 fi
 
-if [ ! -f "/sys/class/gpio/gpio$GPIO_SYSFS/direction" ]; then
-  echo "out" > /sys/class/gpio/gpio$GPIO_SYSFS/direction
-elif [ $(cat /sys/class/gpio/gpio$GPIO_SYSFS/direction) == "in" ]; then
+if [ ! -f "/sys/class/gpio/gpio${GPIO_SYSFS}/direction" ]; then
+  echo "out" > /sys/class/gpio/gpio${GPIO_SYSFS}/direction
+elif [ $(cat /sys/class/gpio/gpio${GPIO_SYSFS}/direction) == "in" ]; then
   # on bascule la direction sur out, ce qui va provoquer un switch
   # on ignorera donc la commande de switch via $normal
   normal=false
-  echo "out" > /sys/class/gpio/gpio$GPIO_SYSFS/direction
+  echo "out" > /sys/class/gpio/gpio${GPIO_SYSFS}/direction
 fi
 
-if [ -z "$2" ]; then
+if [ -z "${2}" ]; then
   # On vérifie si le gpio a été mal fermé (avec sa valeur active, sans unexport), le positionnement sur out a déjà switché, on évite de re-switcher
   if $normal; then
-    if [ $(cat /sys/class/gpio/gpio$GPIO_SYSFS/value) == "0" ]; then
-      echo "1" > /sys/class/gpio/gpio$GPIO_SYSFS/value
+    if [ $(cat /sys/class/gpio/gpio${GPIO_SYSFS}/value) == "0" ]; then
+      echo "1" > /sys/class/gpio/gpio${GPIO_SYSFS}/value
     else
-      echo "0" > /sys/class/gpio/gpio$GPIO_SYSFS/value
+      echo "0" > /sys/class/gpio/gpio${GPIO_SYSFS}/value
     fi
   fi
 else
-  if [ "$2" == "1" ]; then
-    echo "0" > /sys/class/gpio/gpio$GPIO_SYSFS/value
-  elif [ "$2" == "0" ]; then
-    echo "1" > /sys/class/gpio/gpio$GPIO_SYSFS/value
+  if [ "${2}" == "1" ]; then
+    echo "0" > /sys/class/gpio/gpio${GPIO_SYSFS}/value
+  elif [ "${2}" == "0" ]; then
+    echo "1" > /sys/class/gpio/gpio${GPIO_SYSFS}/value
   else
-    echo "Argument $2 incompris"
+    echo "Argument ${2} incompris"
   fi
 fi
