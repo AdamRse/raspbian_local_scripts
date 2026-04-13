0a0554861a42c7888e30821d3df56926a62258e4
AdamRse <adam.rousselle@proton.me>
2026-04-13 19:31:23 +0200

    SET GLOBALS

--- lampeur.sh
+++ lampeur.sh
@@ -30,34 +30,40 @@ lout "--- Paramètres ---"
 lout "GPIO : ${GPIO_ID}"
 lout "Script de basculement : ${SWITCH_SCRIPT_PATH}"
 
+UT_NOW=""
+UT_SUNSET=""
+UT_TWILIGHT=""
+USER_DELAY_SEC=""
+WEATHER_DELAY_SEC=""
+
 while $run; do
     # --- CALCUL DES VALEURS ---
     tab_options=$(get_opt) || trigger_error_delay "Impossible de récupérer les options dans la table mysql, envoi du signal et attente de 1 heure."
     debug_ "tab_options:'${tab_options}'"
-    read user_delay_sec run_mode <<< "${tab_options}"
+    read USER_DELAY_SEC run_mode <<< "${tab_options}"
     [[ $run_mode =~ ^[0-2]$ ]] ||trigger_error_delay "Impossible de récupérer les le run_mode dans la table mysql (lampe_run_skip = '${run_mode}'), envoi du signal et attente de 1 heure."
-    [[ $user_delay_sec =~ ^[0-9]{1,5}$ ]] ||trigger_error_delay "Impossible de récupérer les le user_delay_sec dans la table mysql (lampe_decallage = '${user_delay_sec}'), envoi du signal et attente de 1 heure."
+    [[ $USER_DELAY_SEC =~ ^[0-9]{1,5}$ ]] ||trigger_error_delay "Impossible de récupérer les le USER_DELAY_SEC dans la table mysql (lampe_decallage = '${USER_DELAY_SEC}'), envoi du signal et attente de 1 heure."
 
     today_schedule=$(get_todays_schedule) || trigger_error_delay "Impossible de récupérer les horaires dans la table mysql, envoi du signal et attente de 1 heure."
     debug_ "today_schedule:'${today_schedule}'"
     read sunset_hour_24 twilight_hour_24 <<< "${today_schedule}"
 
-    ut_now=$(date +%s)
-    ut_sunset=$(date -d $sunset_hour_24 +%s)
-    ut_twilight=$(date -d $twilight_hour_24 +%s)
-    if [[ ! $ut_now =~ ^[0-9]{10}$ ]] || [[ ! $ut_sunset =~ ^[0-9]{10}$ ]] || [[ ! $ut_twilight =~ ^[0-9]{10}$ ]]; then
-        trigger_error_delay "Le calcul du temps unix n'a pas fonctionné : ut_now:${ut_now}, ut_sunset:${ut_sunset}, ut_twilight:${ut_twilight}. Envoi du signal et attente de 1 heure."
+    UT_NOW=$(date +%s)
+    UT_SUNSET=$(date -d $sunset_hour_24 +%s)
+    UT_TWILIGHT=$(date -d $twilight_hour_24 +%s)
+    if [[ ! $UT_NOW =~ ^[0-9]{10}$ ]] || [[ ! $UT_SUNSET =~ ^[0-9]{10}$ ]] || [[ ! $UT_TWILIGHT =~ ^[0-9]{10}$ ]]; then
+        trigger_error_delay "Le calcul du temps unix n'a pas fonctionné : UT_NOW:${UT_NOW}, UT_SUNSET:${UT_SUNSET}, UT_TWILIGHT:${UT_TWILIGHT}. Envoi du signal et attente de 1 heure."
     fi
 
-    weather_delay=0
+    WEATHER_DELAY_SEC=0
     if clouds_percent=$(get_clouds_percent_from_weather_api); then
         lout "- - Nuages aujourd'hui : ${clouds_percent}% - -"
-        if ! weather_delay=$(get_delay_from_clouds_percent ${clouds_percent}); then
+        if ! WEATHER_DELAY_SEC=$(get_delay_from_clouds_percent ${clouds_percent}); then
             wout "La récupération du délai en fonction de la météo a échouée, elle est forcée à 0 pour la suite."
-            weather_delay=0
+            WEATHER_DELAY_SEC=0
         fi
     fi
-    lout "Décallage lié au temps actuel : ${weather_delay}s"
+    lout "Décallage lié au temps actuel : ${WEATHER_DELAY_SEC}s"
     # --- DÉBUT DE LA LOGIQUE ---
 
     # 0 = on ne skip pas
