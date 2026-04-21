fa723286445cb90e05190edd268a55299e0092c0
AdamRse <adam.rousselle@proton.me>
2026-04-21 11:56:40 +0200

    Fix template mysql

--- web/app_lampe/env.mysqli.example.php
+++ web/app_lampe/env.mysqli.example.php
@@ -1,9 +1,4 @@
 <?php
 // Ajouter un env.mysqli.php qui initialise la connexion mysqli comme ce qui suit.
 // La base de données à installer est disponible dans ./bdd/raspi_general.sql
-$mysqli = new mysqli(
-    "host",
-    "user",
-    "password",
-    "raspi_general",
-);
+$mysqli = new mysqli("host", "user", "password", "raspi_general");
